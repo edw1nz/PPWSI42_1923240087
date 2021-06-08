f08cf0a225daf0085fbba074fdb0c164c3339ba5
Edwin <edwinvandesar5@gmail.com>
2021-06-08 14:17:50 +0700

    Belajar DB Facade, Query Builder, dan Eloquent

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\http\Controllers\MahasiswaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,41 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//mengirim data ke view
+Route::get("/hallo", function(){
+    $data = ['nama' => 'edwin', 'npm' => '1923240087', 'alamat' => 'Palembang'];
+    return view("hallo", $data);
+});
+
+//menerima data/parameter dan menampilkannya di view
+Route::get("/kenalan/{nama}/{npm}", function($nama, $npm){
+    $data = ['nama' => $nama, 'npm' => $npm];
+    return view("hallo", $data);
+});
+
+
+//Route ke halaman fakultas
+Route::get("/fakultas", function(){
+    $data = ["fakultas" => ["Fasilkom Rekayasa", "Fak. Ekonomi dan Bisnis"]];
+    return view("fakultas.index", $data);
+});
+
+
+//Route Persiapan DB Facade
+Route::get('/mahasiswa/insert', [MahasiswaController::class, 'insert']);
+Route::get('/mahasiswa/update', [MahasiswaController::class, 'update']);
+Route::get('/mahasiswa/delete', [MahasiswaController::class, 'delete']);
+Route::get('/mahasiswa/select', [MahasiswaController::class, 'select']);
+
+//Persiapan Query Builder
+Route::get('/mahasiswa/insert-qb', [MahasiswaController::class, 'insertQb']);
+Route::get('/mahasiswa/update-qb', [MahasiswaController::class, 'updateQb']);
+Route::get('/mahasiswa/delete-qb', [MahasiswaController::class, 'deleteQb']);
+Route::get('/mahasiswa/select-qb', [MahasiswaController::class, 'selectQb']);
+
+//Persiapan Eloquent
+Route::get('/mahasiswa/insert-elq', [MahasiswaController::class, 'insertElq']);
+Route::get('/mahasiswa/update-elq', [MahasiswaController::class, 'updateElq']);
+Route::get('/mahasiswa/delete-elq', [MahasiswaController::class, 'deleteElq']);
+Route::get('/mahasiswa/select-elq', [MahasiswaController::class, 'selectElq']);
